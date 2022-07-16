ee5ebe851ef4326f30d497035e6041466a0586eb
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2022-07-16 12:11:42 +0600

    added economic dashboard yearly graph

--- dashapp.py
+++ dashapp.py
@@ -2,6 +2,8 @@ from dash import dash, dcc, html
 from database import user_collection, economic_collection, \
     business_collection, industry_collection
 from dash.dependencies import Input, Output, State
+import plotly.express as px
+import numpy as np
 
 
 economic_indicator = sorted(economic_collection['Indicator'].unique())
@@ -38,9 +40,18 @@ app.layout= html.Div([
         html.Div([
             html.Div([
                 html.H3("  Economic Dashboard", style={'font-size': 25, 'textAlign': 'left'}),
-                html.H5("  Here you can select any Indicator to see the analysis")
+                html.H5("  Here you can select any Indicator to see the analysis"),
+                html.Br(),
+                html.Br(),
+                dcc.Dropdown(id='Economic_Indicator', options=[{'label':i, 'value': i} for i in economic_indicator], value='Select')
             ], className = "five columns"),
-            dcc.Graph(id="Economic Graph", className="seven columns")
+            html.Div([
+                dcc.Tabs([
+                    dcc.Tab(label='Yearly', children=[dcc.Graph(id="Economic_Graph")]),
+                    dcc.Tab(label='Budget Yearly', children=[dcc.Graph(id="Economic Graph1")]),
+                    dcc.Tab(label='Monthly', children=[dcc.Graph(id="Economic Graph2")])
+                ])
+            ], className = "seven columns")
         ], className='row')
      ]),
      html.Br(),
@@ -63,6 +74,22 @@ app.layout= html.Div([
     ])
 
 
+
+@app.callback(
+    Output('Economic_Graph', 'figure'),
+    Input('Economic_Indicator', 'value'))
+def update_yearly_Economic_graph(Economic_Indicator):
+    data = economic_collection[economic_collection['Indicator'] == Economic_Indicator]
+    CalenderYearData = data[~data['Calendar Value'].isnull()]
+    cdata = CalenderYearData[['Calendar Year', 'Calendar Value']]
+    cdata = cdata.sort_values('Calendar Year')
+    cdata['Calendar Year'] = cdata['Calendar Year'].map(str)
+    figure = px.bar(cdata, x='Calendar Year', y='Calendar Value', text="Calendar Value")
+    figure.update_layout(title={'text':Economic_Indicator,  'y':0.93,
+            'x':0.5,'xanchor':'center', 'yanchor':'top'}, xaxis_title="Source: " + str(np.unique(CalenderYearData['Source'])[0]), yaxis_title="Unit of Measurement: " + str(np.unique(CalenderYearData['Unit'])[0]))
+    for data in figure.data:
+        data["width"] = 0.5
+    return figure
 
 if __name__ == '__main__':
     app.run_server(debug=True)
